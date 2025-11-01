f012a2979a0ef4895124bd7cc763973c4199c068
Tony Horrobin <17624950+triblatron@users.noreply.github.com>
2025-11-01 11:35:49 +0000

    build,fix:Add missing DataTemplate.h test,data:Add more tests for ParameterLookup::iterpolate(). - non-existent parameter, one parameter, two parameters, two parameters with one non-existent. feat:ParameterLookup:Implement configure() and interpolate(). feat:Atom:Add begin() and end() so that we can do a range-based for loop over an Atom.

---
+++ include/core/DataTemplate.h
@@ -0,0 +1,49 @@
+//
+// Created by Tony Horrobin on 28/10/2025.
+//
+
+#pragma once
+
+#include "config/Export.h"
+
+#include "core/Atom.h"
+#include "core/Widget.h"
+#include "util/ParameterLookup.h"
+
+namespace dagbase
+{
+    class ConfigurationElement;
+}
+
+namespace dagui
+{
+    class ShapeFactory;
+    class WidgetFactory;
+
+    class DAGUI_API DataTemplate
+    {
+    public:
+        DataTemplate() = default;
+
+        void setParameter(dagbase::Atom name, dagbase::Variant value)
+        {
+            if (auto it=_paramLookup.find(name); it!=_paramLookup.end())
+            {
+                it->second = value;
+            }
+            else
+            {
+                _paramLookup.emplace(name,value);
+            }
+        }
+
+        void configure(dagbase::ConfigurationElement& config, WidgetFactory& factory, ShapeFactory& shapeFactory);
+
+        void resolve();
+    private:
+        dagbase::Atom _name;
+        dagbase::Atom _dataType;
+        Widget* _tree{nullptr};
+        dagbase::ParameterLookup _paramLookup;
+    };
+}
