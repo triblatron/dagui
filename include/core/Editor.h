f594a5bdfcebcc20df4cb73442b08214132555cd
Tony Horrobin <17624950+triblatron@users.noreply.github.com>
2026-05-02 15:45:43 +0100

    doc:Add some API docs for Editor.

--- include/core/Editor.h
+++ include/core/Editor.h
@@ -15,10 +15,16 @@ namespace dagui
     class DAGUI_API Editor
     {
     public:
+        //! We have virtual functions, so a virtual dtor helps avoid problems
         virtual ~Editor() = default;
 
+        //! Clone this object using its copy ctor
+        //! \note Supports the Prototype pattern in EditorRegistry
         virtual Editor* clone() = 0;
 
+        //! Find an attribute
+        //! \see dagbase::findEndpoint
+        //! \see dagbase::findInternal
         virtual dagbase::Variant find(std::string_view path) const = 0;
     };
 }
