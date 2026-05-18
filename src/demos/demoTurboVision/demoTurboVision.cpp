10365911ae0c72c932eca0f0713d66c340377392
Tony Horrobin <17624950+triblatron@users.noreply.github.com>
2026-05-18 21:30:26 +0100

    demo:Add demoTurboVision to create a window displaying a traditional greeting.

---
+++ src/demos/demoTurboVision/demoTurboVision.cpp
@@ -0,0 +1,136 @@
+//
+// Created by Tony Horrobin on 18/05/2026.
+//
+
+#define Uses_TApplication
+#define Uses_TKeys
+#define Uses_TSubMenu
+#define Uses_TMenuBar
+#define Uses_TStatusLine
+#define Uses_TStatusItem
+#define Uses_TStatusDef
+#define Uses_TWindow
+#define Uses_TDeskTop
+
+#include <tvision/tv.h>
+
+#include "tvision/menus.h"
+#include "tvision/internal/ansiwrit.h"
+#include "tvision/internal/ansiwrit.h"
+
+class TInterior : public TView
+{
+public:
+    TInterior(const TRect& bounds);
+    void draw() override;
+};
+
+TInterior::TInterior(const TRect& bounds)
+    :
+TView(bounds)
+{
+    growMode = gfGrowHiX | gfGrowHiY;
+    options |= ofFramed;
+}
+
+void TInterior::draw()
+{
+    const char* str = "Hello World!";
+    ushort colour = getColor(0x0301);
+    TView::draw();
+    TDrawBuffer b;
+    b.moveStr( 0, str, colour );
+    writeLine( 4, 2, 12, 1, b);
+}
+class TDemoWindow : public TWindow
+{
+public:
+    TDemoWindow( const TRect& r, const char* title, short number );
+};
+
+TDemoWindow::TDemoWindow(const TRect &bounds, const char *title, short number)
+    :
+TWindowInit( &TDemoWindow::initFrame),
+TWindow(bounds, title, number)
+{
+    TRect r = getClipRect();
+    r.grow(-1,-1);
+    insert( new TInterior( r ) );
+}
+
+class TDemoTurboVision : public TApplication
+{
+public:
+    TDemoTurboVision();
+
+    void createWindow();
+    void handleEvent( TEvent& e );
+
+    static TMenuBar* initMenuBar(TRect r);
+    static TStatusLine* initStatusLine(TRect r);
+    static short winNumber;
+};
+
+short TDemoTurboVision::winNumber = 0;
+const int cmNewWin = 199;
+
+TDemoTurboVision::TDemoTurboVision()
+    :
+TProgInit(&TDemoTurboVision::initStatusLine, &TDemoTurboVision::initMenuBar, &TDemoTurboVision::initDeskTop)
+{
+    // Do nothing.
+}
+
+void TDemoTurboVision::createWindow()
+{
+    TRect r(0, 0, 26, 7);
+    r.move( 20, 20 );
+    auto* window = new TDemoWindow( r, "TDemoWindow", ++winNumber );
+    deskTop->insert(window);
+}
+
+void TDemoTurboVision::handleEvent(TEvent &event)
+{
+    TApplication::handleEvent(event);
+    if (event.what == evCommand)
+    {
+        switch (event.message.command)
+        {
+            case cmNewWin:
+                createWindow();
+                break;
+            default:
+                return;
+        }
+        clearEvent(event);
+    }
+}
+
+TMenuBar * TDemoTurboVision::initMenuBar(TRect r)
+{
+    r.b.y = r.a.y + 1;
+    return new TMenuBar( r,
+        *new TSubMenu("~F~ile", kbAltF) +
+            *new TMenuItem( "~O~pen", cmFileOpen, kbF3, hcNoContext, "F3") +
+            *new TMenuItem( "~N~ew", cmNewWin, kbF4, hcNoContext, "F4")
+            );
+}
+
+TStatusLine * TDemoTurboVision::initStatusLine(TRect r)
+{
+    r.a.y = r.b.y - 1;
+    return new TStatusLine(r,
+        *new TStatusDef( 0, 0xffff ) +
+        *new TStatusItem( "~Alt-X~ Exit", kbAltX, cmQuit ) +
+        *new TStatusItem( "~Alt-F3~ Close", kbAltF3, cmClose )
+        );
+}
+
+int main()
+{
+    TDemoTurboVision app;
+
+    app.run();
+
+    return 0;
+}
